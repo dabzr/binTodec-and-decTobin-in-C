c6557bc31e3d631a717efeb3f7e654598cd403e8
dylanabzr <dylanab@pm.me>
2023-09-10 07:53:57 -0300

    Improving the code

--- binary.c
+++ binary.c
@@ -1,42 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
 
 int binaryToDecimal(char *bin){
     int i, j, dec =0;
-    char num[3] = "01";
+    char num[2] = "1";
     for (i=strlen(bin);i>=0;i--){
-        if (bin[i] == num[1]){
-            dec = dec + (pow(2, j-1));}
+        if (bin[i] == num[0])
+            dec = dec + (1 << (j-1));
         j++;
     }
     return dec;
 }
 
 char * decimalToBinary(int dec){
-    int i = 0;
     char *bin = malloc(sizeof(double));
-    char one[2] = "1";
-    char tempchar[50];
-    while (i==0){
+    char tempchar[2];
+    while (1){
         if (dec == 1){
-            strcat(bin, one);
+            strcat(bin, "1");
             break;
         }
         sprintf(tempchar, "%d", dec%2);
         strcat(bin, tempchar);
-        dec = dec / 2;
+        dec /= 2;
     }
     char *tmp = malloc(sizeof(char) * strlen(bin));
-    for (int i = 0; i <= strlen(bin); i++){
-    tmp[i] = bin[strlen(bin)-i-1];
-  }
+    for (int i = 0; i <= strlen(bin); i++)
+      tmp[i] = bin[strlen(bin)-i-1];
     strcpy(bin, tmp);
     free(tmp);
     return bin;
 }
 int main (){
     printf("%d\n", binaryToDecimal("100000"));
-    printf("%s\n", decimalToBinary(4));
+    printf("%s\n", decimalToBinary(127));
 }
